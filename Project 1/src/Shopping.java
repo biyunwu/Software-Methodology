80f3fb62603bd75b653bb2e1d6b6af37a532813e
Biyun Wu <beenwu2016@gmail.com>
2020-09-11 23:28:37 -0400

    Optimize ShoppingBag

--- Project 1/src/Shopping.java
+++ Project 1/src/Shopping.java
@@ -2,90 +2,74 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
- * 
  * @authors Anthony Triolo and Biyun Wu
- *
  */
+
 public class Shopping {
 	public void run() {
 		System.out.println("Let's start shopping!");
 		ShoppingBag bag = new ShoppingBag();
 		Scanner in = new Scanner(System.in);
-		String input;
-		while ((input = in.nextLine()).equals("Q") == false) {
-			String[] seperatedInput = input.split(" ");
-			switch (seperatedInput[0]) {
-			case "A":
-				if (seperatedInput.length < 4) {
-					System.out.println("Invalid command!");
-					break;
+		String input = in.nextLine();
+		while (!input.equals("Q")) {
+			String[] separatedInput = input.split(" ");
+			switch (separatedInput.length) {
+				case 1 -> {
+					switch (separatedInput[0]) {
+						case "P" -> {
+							if (bag.getSize() == 0) {
+								System.out.println("The bag is empty!");
+							} else {
+								System.out.println("**You have " + bag.getSize() + " items in the bag.");
+								bag.print();
+								System.out.println("**End of list");
+							}
+						}
+						case "C" -> {
+							if (bag.getSize() == 0) {
+								System.out.println("Unable to check out, the bag is empty!");
+							} else {
+								System.out.println("Checking out " + bag.getSize() + " items.");
+								bag.print();
+								DecimalFormat df = new DecimalFormat("0.00");
+								double salesPrice = bag.salesPrice();
+								System.out.println("*Sales total: $" + df.format(salesPrice));
+								double salesTax = bag.salesTax();
+								System.out.println("*Sales tax: $" + df.format(salesTax));
+								double totalPrice = salesPrice + salesTax;
+								System.out.println("*Total amount paid: $" + df.format(totalPrice));
+								bag = new ShoppingBag();
+							}
+						}
+						default -> System.out.println("Invalid command!");
+					}
 				}
-				String newitemName = seperatedInput[1];
-				double newitemPrice = Double.parseDouble(seperatedInput[2]);
-				boolean newitemTaxable = Boolean.valueOf(seperatedInput[3]);
-				GroceryItem newItem = new GroceryItem(newitemName, newitemPrice, newitemTaxable);
-				bag.add(newItem);
-				System.out.println(newitemName + " added to the bag");
-				break;
-			case "R":
-				if (seperatedInput.length < 4) {
-					System.out.println("Invalid command!");
-					break;
+				case 4 -> {
+					String itemName = separatedInput[1];
+					double itemPrice = Double.parseDouble(separatedInput[2]);
+					boolean itemTaxable = Boolean.parseBoolean(separatedInput[3]);
+					GroceryItem itemObj = new GroceryItem(itemName, itemPrice, itemTaxable);
+					switch (separatedInput[0]) {
+						case "A" -> {
+							bag.add(itemObj);
+							System.out.println(itemName + " added to the bag");
+						}
+						case "R" -> {
+							if (!bag.remove(itemObj)) {
+								System.out.println("Unable to remove, this item is not in the bag.");
+							} else {
+								System.out.println(itemName + " " + itemPrice + " removed.");
+							}
+						}
+						default -> System.out.println("Invalid command!");
+					}
 				}
-				String removeitemName = seperatedInput[1];
-				double removeitemPrice = Double.parseDouble(seperatedInput[2]);
-				boolean removeitemTaxable = Boolean.valueOf(seperatedInput[3]);
-				GroceryItem removeItem = new GroceryItem(removeitemName, removeitemPrice, removeitemTaxable);
-				if (bag.remove(removeItem) == false) {
-					System.out.println("Unable to remove, this item is not in the bag.");
-					break;
-				} else {
-					System.out.println(removeitemName + " " + removeitemPrice + " removed.");
-					break;
-				}
-			case "P":
-				if (bag.getSize() == 0) {
-					System.out.println("The bag is empty!");
-					break;
-				}
-				System.out.println("**You have " + bag.getSize() + " items in the bag.");
-				bag.print();
-				System.out.println("**End of list");
-				break;
-			case "C":
-				if (bag.getSize() == 0) {
-					System.out.println("Unable to check out, the bag is empty!");
-					break;
-				}
-				System.out.println("Checking out " + bag.getSize() + " items.");
-				bag.print();
-				DecimalFormat df = new DecimalFormat("0.00");
-				double salesPrice = bag.salesPrice();
-				System.out.println("*Sales total: $" + df.format(salesPrice));
-				double salesTax = bag.salesTax();
-				System.out.println("*Sales tax: $" + df.format(salesTax));
-				double totalPrice = salesPrice + salesTax;
-				System.out.println("*Total amount paid: $" + df.format(totalPrice));
-				bag = new ShoppingBag();
-				break;
-			default:
-				System.out.println("Invalid command!");
-				break;
+				default -> System.out.println("Invalid command!");
 			}
+			input = in.nextLine();
 		}
-		if (bag.getSize() != 0) {
-			System.out.println("Checking out " + bag.getSize() + " items.");
-			bag.print();
-			DecimalFormat df = new DecimalFormat("0.00");
-			double salesPrice = bag.salesPrice();
-			System.out.println("*Sales total: $" + df.format(salesPrice));
-			double salesTax = bag.salesTax();
-			System.out.println("*Sales tax: $" + df.format(salesTax));
-			double totalPrice = salesPrice + salesTax;
-			System.out.println("*Total amount paid: $" + df.format(totalPrice));
-		}
+
 		System.out.println("Thanks for shopping with us!");
 		in.close();
-		return;
 	}
 }
--- Project 1/src/ShoppingBag.java
+++ Project 1/src/ShoppingBag.java
@@ -1,56 +1,63 @@
 /**
- * 
  * @authors Anthony Triolo and Biyun Wu
- *
  */
+
 public class ShoppingBag {
 	private GroceryItem[] bag;
 	private int size;
 	private int capacity;
 
-	public ShoppingBag() {
-
-		this.bag = new GroceryItem[5];
+	public ShoppingBag() { // Constructor
+		int INITIAL_CAPACITY = 5;
+		this.bag = new GroceryItem[INITIAL_CAPACITY];
 		this.capacity = bag.length;
 		this.size = 0;
 	}
 
+	/**
+	 * Helper method to find an item.
+	 * @param item: GroceryItem to be searched in ShoppingBag.
+	 * @return index of the item or -1 if not found.
+	 */
 	private int find(GroceryItem item) {
 		for (int i = 0; i < size; i++) {
-			if (bag[i].equals(item) == true) {
+			if (bag[i].equals(item)) {
 				return i;
 			}
 		}
 		return -1;
 	}
 
-	private void grow() {
-		GroceryItem[] tempBag = new GroceryItem[capacity + 1];
-		for (int i = 0; i < capacity; i++) {
-			tempBag[i] = bag[i];
-		}
+	private void grow() { // Helper method to grow the capacity.
+		int INCREMENT = 5; // "If the bag is full, the bag automatically grows the capacity by 5."
+		GroceryItem[] tempBag = new GroceryItem[capacity + INCREMENT];
+		if (capacity >= 0)
+			System.arraycopy(bag, 0, tempBag, 0, capacity);
 		bag = tempBag;
 		capacity++;
 	}
 
+	/**
+	 * @param item to be added to ShoppingBag
+	 */
 	public void add(GroceryItem item) {
 		if (size == capacity) {
 			grow();
 		}
-		for (int i = 0; i < capacity; i++) {
-			if (bag[i] == null) {
-				bag[i] = item;
-				size++;
-				return;
-			}
-		}
+		bag[size] = item; // Index of `size` is always the first position available to add new item.
+		size++;
 	}
 
+	/**
+	 * Find the target in `GroceryItem[] bag` and replace it with the last no-NULL object in the array.
+	 * @param item object to be removed in the bag.
+	 * @return success -> true / fail -> false.
+	 */
 	public boolean remove(GroceryItem item) {
-		if (find(item) == -1) {
+		int removeIndex = find(item);
+		if (removeIndex == -1) {
 			return false;
 		}
-		int removeIndex = find(item);
 		bag[removeIndex] = bag[size - 1];
 		bag[size - 1] = null;
 		size--;
@@ -66,16 +73,17 @@ public class ShoppingBag {
 	}
 
 	public double salesTax() {
+		double TAX_RATE = 0.06625;
 		double totalTax = 0.0;
 		for (int i = 0; i < size; i++) {
-			if (bag[i].getTaxable() == true) {
-				totalTax += (bag[i].getPrice() * 0.06625);
+			if (bag[i].getTaxable()) {
+				totalTax += (bag[i].getPrice() * TAX_RATE);
 			}
 		}
 		return totalTax;
 	}
 
-	public void print() {
+	public void print() { // Print items in bag.
 		for (int i = 0; i < size; i++) {
 			System.out.println("\u2022 " + bag[i].toString());
 		}
@@ -84,5 +92,4 @@ public class ShoppingBag {
 	public int getSize() {
 		return size;
 	}
-
 }
