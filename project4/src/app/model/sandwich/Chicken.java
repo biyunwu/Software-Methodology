c3ef4da8a4a1f62f787f61601f1586cc724e4a4b
ajtriolo1 <62657157+ajtriolo1@users.noreply.github.com>
2020-11-10 16:59:49 -0500

    Merged Code (#46)
    * Zipped up project
    
    * Fixed bullet points
    
    * Rewrote TransactionManager and fixed extra withdrawals
    
    * Changed how date is read
    
    * Update AccountDatabase.java
    
    * Added some comments
    
    * Added JUnit tests and javadoc
    
    * Updated DateTest and closed scanner on quit
    
    * Fixed DateTest
    
    * Added main comment
    
    * Started project 3
    
    * Improved import
    
    * Improved import
    
    * Fixed bugs
    
    * Merge master (#35)
    
    * Added main comment (#33)
    
    * Zipped up project
    
    * Fixed bullet points
    
    * Rewrote TransactionManager and fixed extra withdrawals
    
    * Changed how date is read
    
    * Update AccountDatabase.java
    
    * Added some comments
    
    * Added JUnit tests and javadoc
    
    * Updated DateTest and closed scanner on quit
    
    * Fixed DateTest
    
    * Added main comment
    
    * add project 3 (#34)
    
    * replace numbers by constant variables, reuse find() in withdrawal()
    
    * rename/delete variables
    
    * mergesort
    
    * remove extra call on debit() in printDetail()
    
    * tab1: open and close account
    
    * tab2: deposit and withdraw
    
    * update interface
    
    * modify event listeners and update interface
    
    * tab3: print methods and import to database
    
    * tab3: implement export
    
    * one controller and one view
    
    * clean fxml file
    
    * add comments
    
    * optimize controller functions for tab 1
    
    * check user name, change error message
    
    * add missing comment
    
    * delete duplicate code
    
    Co-authored-by: Biyun Wu <beenwu2016@gmail.com>
    
    * Remove local project
    
    * Removed local folder
    
    * Fixed punctuation and comments
    
    * Fixed punctuation and comments (#36) (#37)
    
    * Zipped up project
    
    * Fixed bullet points
    
    * Rewrote TransactionManager and fixed extra withdrawals
    
    * Changed how date is read
    
    * Update AccountDatabase.java
    
    * Added some comments
    
    * Added JUnit tests and javadoc
    
    * Updated DateTest and closed scanner on quit
    
    * Fixed DateTest
    
    * Added main comment
    
    * Started project 3
    
    * Improved import
    
    * Improved import
    
    * Fixed bugs
    
    * Merge master (#35)
    
    * Added main comment (#33)
    
    * Zipped up project
    
    * Fixed bullet points
    
    * Rewrote TransactionManager and fixed extra withdrawals
    
    * Changed how date is read
    
    * Update AccountDatabase.java
    
    * Added some comments
    
    * Added JUnit tests and javadoc
    
    * Updated DateTest and closed scanner on quit
    
    * Fixed DateTest
    
    * Added main comment
    
    * add project 3 (#34)
    
    * replace numbers by constant variables, reuse find() in withdrawal()
    
    * rename/delete variables
    
    * mergesort
    
    * remove extra call on debit() in printDetail()
    
    * tab1: open and close account
    
    * tab2: deposit and withdraw
    
    * update interface
    
    * modify event listeners and update interface
    
    * tab3: print methods and import to database
    
    * tab3: implement export
    
    * one controller and one view
    
    * clean fxml file
    
    * add comments
    
    * optimize controller functions for tab 1
    
    * check user name, change error message
    
    * add missing comment
    
    * delete duplicate code
    
    Co-authored-by: Biyun Wu <beenwu2016@gmail.com>
    
    * Remove local project
    
    * Removed local folder
    
    * Fixed punctuation and comments
    
    Co-authored-by: Biyun Wu <beenwu2016@gmail.com>
    
    Co-authored-by: Biyun Wu <beenwu2016@gmail.com>
    
    * Fixed comment over openAccount
    
    * Added error for importing existing accounts
    
    * Fixed input checking for import feature
    
    * Fixed command throwing exception
    
    * Put Application in source folder and fixed exception in import
    
    * Fixed exception in import, added test doc
    
    * Set up project and added implementation of model
    
    * Removed local files
    
    * Created first page
    
    * Added first page
    
    * Added ability to add sandwich to order and created order screen
    
    * Made sandwich selection a combobox
    
    * Fixed trailing decimals
    
    * Added back button
    
    * Merged code
    
    * Merged code
    
    Co-authored-by: Biyun Wu <beenwu2016@gmail.com>

--- project4/src/app/model/sandwich/Chicken.java
+++ project4/src/app/model/sandwich/Chicken.java
@@ -1,27 +1,53 @@
 package app.model.sandwich;
 
+/**
+ * Definition of the Beef class. It extends the Sandwich class and has one member variable:
+ * the base price of a chicken sandwich
+ *
+ * @author Biyun Wu, Anthony Triolo
+ */
+
 import java.util.ArrayList;
 
-public class Chicken extends Sandwich{
+public class Chicken extends Sandwich {
 	private static final double BASE_PRICE = 8.99;
 
+	/**
+	 * Constructor
+	 */
 	public Chicken() {
 		extras = new ArrayList<>();
 	}
 
+	/**
+	 * Calculates the price of a chicken sandwich with extra ingredients
+	 * 
+	 * @return The total price of a chicken sandwich
+	 */
 	@Override
 	public double price() {
 		return BASE_PRICE + PER_EXTRA * extras.size(); // if null exists in ArrayList, does it count to size?
 	}
 
+	/**
+	 * Stringify the information about a chicken sandwich
+	 * 
+	 * @return The formatted string of information
+	 */
 	@Override
 	public String toString() {
 		String baseIngres = "Base: " + String.join(", ", this.getBaseIngredients()) + ". ";
-		String extraIngres = "Extra: " + super.toString() + ". ";
-		return "Chicken Sandwich. " + baseIngres + extraIngres;
+		String extraIngres = "Extras: " + super.toString() + ". ";
+		String price = "Price: $" + price();
+		return "Chicken Sandwich. " + baseIngres + extraIngres + price;
 	}
 
+	/**
+	 * Helper method to get the base ingredients of a chicken sandwich
+	 * 
+	 * @return An array of the base ingredients
+	 */
 	public String[] getBaseIngredients() {
-		return new String[]{"Fried Chicken", "Spicy Sauce", "Pickles"};
+		return new String[] { "Fried Chicken", "Spicy Sauce", "Pickles" };
 	}
 }
